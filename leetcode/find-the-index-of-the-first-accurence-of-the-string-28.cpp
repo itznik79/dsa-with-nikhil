2ca3aada6850c0b2071ad9d836c57cb460d5e4f1
Nikhil Gautam <nikhilkgautam97@gmail.com>
2025-10-19 15:33:20 +0530

    2 leetcode

--- leetcode/find-the-index-of-the-first-accurence-of-the-string-28.cpp
+++ leetcode/find-the-index-of-the-first-accurence-of-the-string-28.cpp
@@ -1,1 +1,14 @@
-    
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if (needle == haystack) return 0;
+        if (needle.size() > haystack.size()) return -1;
+
+        for (int i = 0; i <= haystack.size() - needle.size(); i++) {
+            if (haystack.substr(i, needle.length()) == needle) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
